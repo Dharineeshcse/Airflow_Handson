0b7d279b638312be017444626a77a72ad380934a
Dharineeshcse <dharineesh22@gmail.com>
2025-11-14 17:10:23 +0530

    add keda-scaling.py file

--- DAG_files/keda-scaling.py
+++ DAG_files/keda-scaling.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
 # This DAG will generate a large number of scheduled + queued tasks
@@ -15,8 +15,9 @@ with DAG(
     start = EmptyOperator(task_id="start")
 
     # Create 200 lightweight tasks
-    tasks = [EmptyOperator(task_id=f"task_{i}") for i in range(1, 201)]
-
+    tasks = [BashOperator(task_id=f"task_{i}", bash_command='echo "This is another pod running..."') for i in range(1, 201)]
     end = EmptyOperator(task_id="end")
+
+    
 
     start >> tasks >> end
