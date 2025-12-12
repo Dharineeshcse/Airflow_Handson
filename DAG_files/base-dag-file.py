153b06dc32f0ab870872af5d9223ff7d11355192
Dharineeshcse <dharineesh22@gmail.com>
2025-12-12 12:41:04 +0530

    modified bash command values

---
+++ DAG_files/base-dag-file.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="print_hi_dag",
+    start_date=datetime(2025, 1, 1),
+    schedule=None,   # Run manually
+    catchup=False,
+) as dag:
+
+    print_hi = BashOperator(
+        task_id="sample_dag_task_2",
+        bash_command="echo created a ci/cd pipeline"
+    )
